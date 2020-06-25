b6047228357735f382917589fbadae5043e11543
Ashish Padhi <ashish.ashish870@gmail.com>
2020-06-25 12:17:49 +0530

    changed routing name

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DepartmentListComponent } from './department-list/department-list.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/departments', pathMatch: 'full' }, // pathMatch: 'prefix'
-  { path: 'departments', component: DepartmentListComponent },
+  { path: '', redirectTo: '/departments-list', pathMatch: 'full' }, // pathMatch: 'prefix'
+  { path: 'departments-list', component: DepartmentListComponent },
+  { path: 'departments-list/:id', component: DepartmentDetailComponent },
   { path: 'employees', component: EmployeeListComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
@@ -21,5 +23,6 @@ export class AppRoutingModule { }
 export const routingComponents = [
   DepartmentListComponent, 
   EmployeeListComponent,
-  PageNotFoundComponent
+  PageNotFoundComponent,
+  DepartmentDetailComponent
 ]
